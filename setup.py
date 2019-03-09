dc1ae246c0200ff4fa8bbcdb19e734d232c0b5a5
cha chen <cha@drive.ai>
2019-03-09 15:54:26 -0800

    [REFACTOR] two space indent

--- setup.py
+++ setup.py
@@ -1,22 +1,25 @@
 import setuptools
 
 with open("README.md", "r") as fh:
-    long_description = fh.read()
+  long_description = fh.read()
 
 
 setuptools.setup(
-    name="py3_dependecy_graph",
-    version="0.0.1",
-    author="Cha Chen",
-    author_email="chencha92111@gmail.com",
-    description="A simple jedi based python3 dependency analysis tool",
-    long_description=long_description,
-    long_description_content_type="text/markdown",
-    url="https://github.com/jam-world/py3_dependency_graph",
-    packages=setuptools.find_packages(),
-    classifiers=[
-        "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: MIT License",
-        "Operating System :: OS Independent",
-    ],
+  name="py3_dependecy_graph",
+  version="0.0.1",
+  author="Cha Chen",
+  author_email="chencha92111@gmail.com",
+  description="A simple jedi based python3 dependency analysis tool",
+  long_description=long_description,
+  long_description_content_type="text/markdown",
+  url="https://github.com/jam-world/py3_dependency_graph",
+  packages=setuptools.find_packages(),
+  install_requires=[
+    'git'
+  ],
+  classifiers=[
+    "Programming Language :: Python :: 3",
+    "License :: OSI Approved :: MIT License",
+    "Operating System :: OS Independent",
+  ],
 )
